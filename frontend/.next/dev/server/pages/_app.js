98ded822b2c6a43742a64dff715aa12e89118cde
iamjuanyouarenot <iamjuanyouarenot@gmail.com>
2025-12-10 20:58:59 -0600

    chore: ensure all files are committed

--- frontend/.next/dev/server/pages/_app.js
+++ frontend/.next/dev/server/pages/_app.js
@@ -1,4 +1,4 @@
 var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_app.js")
-R.c("server/chunks/ssr/[root-of-the-server]__996d85b1._.js")
-R.m("[project]/gestor-prestamos/frontend/node_modules/next/app.js [ssr] (ecmascript)")
-module.exports=R.m("[project]/gestor-prestamos/frontend/node_modules/next/app.js [ssr] (ecmascript)").exports
+R.c("server/chunks/ssr/[root-of-the-server]__70a73b34._.js")
+R.m("[project]/node_modules/next/app.js [ssr] (ecmascript)")
+module.exports=R.m("[project]/node_modules/next/app.js [ssr] (ecmascript)").exports
